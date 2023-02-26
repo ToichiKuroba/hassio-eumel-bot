24407a171fcff02595a4b46b46d9e08bd30b2374
Frederick Weimann <git@login.f-weimann.com>
2023-02-26 20:26:57 +0100

    write toke in dotenv file

--- discordBot/run.sh
+++ discordBot/run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/with-contenv bashio
 
 discordToken="$(bashio::config 'discord_token')"
-export token=discordToken
 echo "Start Execution"
 cd /discordBot
-echo ls
+echo -e 'token=$discordToken' > .env 
+ls
 npm start
 echo "End Execution"
